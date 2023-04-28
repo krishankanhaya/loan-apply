01f9175d4fcea9a893a61f0a3f3fdecc1af1dcdd
krishankanhaya <krishankanhaya0007@gmail.com>
2023-04-28 11:27:56 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AppBar from './components/AppBar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      <AppBar />
+    </div>
+  );
+}
+
+export default App;
